44f8521e5dd1c5bdda5a3559dd3654f61a660a16
peterrahme <peter.rahme@ocsbstudent.ca>
2021-02-22 18:05:11 +0000

    spacing

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ document.getElementById('button').addEventListener('click', checking)
 let userAge = 0
 let notAWeekend = 0
 
-
 function checking () {
   // Get userAge and converts it to an integer
   userAge = document.getElementById('input').value
   userAge = parseInt(userAge)
   notAWeekend = document.getElementById('enter').value
-  
-
-  if (((notAWeekend === 'monday') || (notAWeekend === 'tuesday') || (notAWeekend === 'wednesday') || (notAWeekend === 'thursday') || (notAWeekend === 'friday')) && (userAge <18)) {
+  // checking what the User day will be
+  if (((notAWeekend === 'monday') || (notAWeekend === 'tuesday') || (notAWeekend === 'wednesday') || (notAWeekend === 'thursday') || (notAWeekend === 'friday')) && (userAge < 18)) {
     document.getElementById('answer').innerHTML = 'Time for school!'
-  } else if (((notAWeekend === 'monday') || (notAWeekend ===   'tuesday') || (notAWeekend === 'wednesday') || (notAWeekend === 'thursday') || (notAWeekend === 'friday')) && (userAge >=18))
+  } else if (((notAWeekend === 'monday') || (notAWeekend ===   'tuesday') || (notAWeekend === 'wednesday') || (notAWeekend === 'thursday') || (notAWeekend === 'friday')) && (userAge >= 18))
    document.getElementById('answer').innerHTML = 'Time to go to work!'
   else {
     document.getElementById('answer').innerHTML = 'Relax for weekend!'
